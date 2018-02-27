e8a2784d84d45a20ced6d98e2e526b457cf4f15c
Teo Laírla <teo.lairla@iessierradeguara.com>
2018-02-27 08:58:09 +0100

    Añade los archivos iniciales del proyecto
    Se añaden las clases Calculadora y Programa

---
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -0,0 +1,11 @@
+package Calculadora;
+
+public class Programa {
+	
+	//TODO: Crear un programa principal que 
+	//utilice todas las funciones de la calculadora
+	//Incluida las operaciones toString y la potencia
+	//que habréis añadido.
+	//Ejecutadlo.
+
+}
